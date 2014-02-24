ffe8808b3bb555919281f23b280d90529d1301b9
knuklz <a.monaghan90@gmail.com>
2014-02-24 14:40:09 +1000

    New project to create a simple click game

---
+++ src/clickgame/Game.java
@@ -0,0 +1,22 @@
+package clickgame;
+
+public class Game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//Define score variables
+		
+		//Create a basic window
+			//objects that make up said game window
+		//gameloop
+		
+		
+		
+		
+		
+		
+	}
+
+}
